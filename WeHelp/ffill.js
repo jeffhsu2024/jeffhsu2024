62a9a5d443f388d54053c3003cb38fcc7c2a107c
jeffhsu2024 <158416667+jeffhsu2024@users.noreply.github.com>
2024-02-25 16:55:36 +0800

    10. 用前一個有效值填滿空字串

---
+++ WeHelp/ffill.js
@@ -0,0 +1,34 @@
+/*
+10. 用前一個有效值填滿空字串
+輸入一個可能包含空字串的字串陣列 / 列表，你的函式能把陣列 / 列表中的空字串用前一個有效值 ( 非空字串 ) 取代。若沒有前一個有效值，則保持空字串不變。
+
+輸入範例一：["", "a", "", "", "c"]
+回傳：["", "a", "a", "a", "c"]
+
+輸入範例二：["a", "b", "", "c", ""]
+回傳：["a", "b", "b", "c", "c"]
+
+輸入範例一：["", "", "a"]
+回傳：["", "", "a"]
+*/
+
+/*
+    @param words:{[String]}
+    @return :{[String]}
+*/
+
+function ffill(words){
+    let temp = "";
+    for(let i=0;i<words.length;i++)
+    {
+        if(words[i]=="")
+        {
+            words[i] = temp;
+        }
+        else
+        {
+            temp = words[i];
+        }
+    }
+    return words;
+}
